160a3d7cd5b6e4091b53430cb37e2c5fb4ffca16
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-03-23 22:37:00 +0100

    update

--- models/course.js
+++ models/course.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+});
 
-
-
-    title:{
-        type:String,
-        require:true
-    }
-})
-
-const model=mongoose.models.course || mongoose.model("Course" , schema)
+const model = mongoose.models.Course || mongoose.model("Course", schema);
 export default model;
